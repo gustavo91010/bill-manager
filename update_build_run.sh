2e3da9d2e1a18408b57f5788a13d496117d8ac83
gustavo91010 <gustavo910@gmail.com>
2023-08-28 23:26:49 -0300

    feat: docker compose ok

--- update_build_run.sh
+++ update_build_run.sh
@@ -5,3 +5,4 @@
 mvn clean package install &&       docker build -t bill-manager_api . &&     docker run -p 8080:8080 bill-manager_api:latest
            
 
+# docker compose up faz a parada toda rodar, com bando  independente
