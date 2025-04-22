e2015f2df988d00b6512fb03dbf97bfdf643e348
Gustavo Paes <gustavo910@gmail.com>
2025-04-22 10:17:04 -0300

    feat: include interceptor for logs in application

--- src/main/java/com/ajudaqui/billmanager/config/FilterRequest.java
+++ src/main/java/com/ajudaqui/billmanager/config/FilterRequest.java
@@ -7,32 +7,25 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@WebFilter(value = "/*", asyncSupported = true) // colocar o path da requisição que recebe arquivos de ate 100 mb
-public class ManagerLog implements Filter {
-
-  private static final Logger logger = LoggerFactory.getLogger(ManagerLog.class);
+// @Configuration
+// @WebFilter(value = "/*", asyncSupported = true) // colocar o path da requisição que recebe arquivos de ate 100 mb
+public class FilterRequest implements Filter {
+  // ele verifica as requisiçoẽs http antes de chegar a classe
+  private static final Logger logger = LoggerFactory.getLogger(FilterRequest.class);
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     String authorization = httpRequest.getHeader("Authorization");
-    System.out.println(httpRequest);
-    // logger.info("[{}] | {} | {} | authorization: {}",
     logger.info("[{}] | {} | authorization: {}",
         httpRequest.getMethod(),
         httpRequest.getRequestURI(),
-        // java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd
-        // HH:mm:ss")),
         authorization);
 
     chain.doFilter(request, response);
---
+++ src/main/java/com/ajudaqui/billmanager/config/RequestLoggingInterceptor.java
@@ -0,0 +1,33 @@
+package com.ajudaqui.billmanager.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+  private static final Logger logger = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+
+    if (handler instanceof HandlerMethod) {
+      HandlerMethod method = (HandlerMethod) handler;
+      String controller = method.getBeanType().getSimpleName();
+      String methodName = method.getMethod().getName();
+      String path = request.getRequestURI();
+      String httpMethod = request.getMethod();
+
+      logger.info("{} | {} : [{}] {}", controller, methodName, httpMethod, path);
+    }
+
+    return true; // continua a execução
+  }
+
+}
