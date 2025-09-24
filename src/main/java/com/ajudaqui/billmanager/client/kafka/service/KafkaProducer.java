f982dc0c391dedcae633da518b4e1eda342533d1
Gustavo Paes <gustavo910@gmail.com>
2025-09-24 09:06:54 -0300

    feat: add circuit breaker in progress

--- src/main/java/com/ajudaqui/billmanager/client/kafka/service/KafkaProducer.java
+++ src/main/java/com/ajudaqui/billmanager/client/kafka/service/KafkaProducer.java
@@ -1,18 +1,48 @@
 package com.ajudaqui.billmanager.client.kafka.service;
 
+import com.ajudaqui.billmanager.client.kafka.controller.KafkaController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Service
 public class KafkaProducer {
   private final KafkaTemplate<String, String> kafkaTemplate;
+  private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+  private static final Logger log = LoggerFactory.getLogger(KafkaController.class);
 
-  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate,
+      Resilience4JCircuitBreakerFactory circuitBreakerFactory) {
     this.kafkaTemplate = kafkaTemplate;
+    this.circuitBreakerFactory = circuitBreakerFactory;
   }
 
   public void sendMessage(String topic, String message) {
-    this.kafkaTemplate.send(topic, message);
+    Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("kafka-producer");
+
+    circuitBreaker.run(() -> {
+      // Tenta enviar para o kafka
+      return this.kafkaTemplate.send(topic, message);
+
+    }, throwable -> {
+      return handleKafkaFallback(topic, message, throwable.getMessage());
+    });
+
+    // this.kafkaTemplate.send(topic, message);
+  }
+
+  private ListenableFuture<SendResult<String, String>> handleKafkaFallback(String topic, String message,
+      String error) {
+    log.error("Erro ao enviar mensagem para Kafka [topic={}]: {}", topic, error);
+
+    return null;
   }
 
 }
---
+++ src/main/java/com/ajudaqui/billmanager/config/CircuitBrakerConfig.java
@@ -0,0 +1,48 @@
+package com.ajudaqui.billmanager.config;
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
+@Configuration
+public class CircuitBrakerConfig {
+
+  @Bean
+  public Customizer<Resilience4JCircuitBreakerFactory> circuitBreakerCustomizer() {
+
+    return factory -> {
+      // Configuração para chamada de api
+      factory.configure(Builder -> Builder
+          .circuitBreakerConfig(CircuitBreakerConfig.custom()
+              .failureRateThreshold(50) // porcentagem de falhas em chamada no periodo de tempo validado
+              .waitDurationInOpenState(Duration.ofSeconds(30)) // tempo que o circuito fica aberto antes de tentar
+                                                               // fechar novamente
+              .slidingWindowSize(10) // tempo de validacao de falha
+              .build()) // tipo, vai ficar aberto por 10s se tiver falha de 50% das request ele abre...
+          .timeLimiterConfig(TimeLimiterConfig.custom()
+              .timeoutDuration(Duration.ofSeconds(5)) // tempo de espera de timeout para abir o circuito
+              .build()),
+          "http-call");
+
+      // configurado para o producer do kafka
+      factory.configure(builder -> builder
+          .circuitBreakerConfig(CircuitBreakerConfig.custom()
+              .failureRateThreshold(60)
+              .waitDurationInOpenState(Duration.ofSeconds(60))
+              .slidingWindowSize(5)
+              .build())
+          .timeLimiterConfig(TimeLimiterConfig.custom()
+              .timeoutDuration(Duration.ofSeconds(5))
+              .build())
+
+          , "kafka-producer");
+    };
+  }
+
+}
